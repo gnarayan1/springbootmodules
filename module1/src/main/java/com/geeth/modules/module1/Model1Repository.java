a95c0d4393763930ca3c308adcfe8af8918e24b5
Geeth Narayanan <gnarayan1@gmail.com>
2017-03-31 13:12:24 -0500

    Added models, repository to both modules

---
+++ module1/src/main/java/com/geeth/modules/module1/Model1Repository.java
@@ -0,0 +1,12 @@
+package com.geeth.modules.module1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.geeth.models.commonmodels.Model1;
+
+public interface Model1Repository extends JpaRepository<Model1, Long> {
+
+}
+
+
+
---
+++ module2/src/main/java/com/geeth/modules/module2/Model2Repository.java
@@ -0,0 +1,12 @@
+package com.geeth.modules.module2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.geeth.models.commonmodels.Model2;
+
+public interface Model2Repository extends JpaRepository<Model2, Long> {
+
+}
+
+
+
